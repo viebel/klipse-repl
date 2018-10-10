ee5e5a258aa0e727fc005682f7b438430acce30d
Yehonathan Sharvit <viebel@gmail.com>
2018-10-10 17:42:52 +0300

    improve

--- src/klipse_repl/eval.clj
+++ src/klipse_repl/eval.clj
@@ -18,29 +18,46 @@
       (println "some cool forms are available in this REPL"))
     (println "cool forms are disabled. Enable them with --cool-forms")))
 
+(defn ns-unmap-in-current-ns [name]
+  (ns-unmap (symbol (str *ns*)) name))
+
+(comment (ns-unmap-in-current-ns 'foo))
 
 (with-test
-  (defn eval-defn [[_ func-name :as =form]]
+  (defn eval-defn [[_ func-name :as form]]
     (try
       (let [func-exists? (resolve func-name)
             created-or-updated (if func-exists? "updated" "created")
             res (eval form)]
         (symbol (str "Function " (:name (meta res)) " " created-or-updated)))
       (catch Exception e
+        (println "an exception occured" e)
         (eval form))))
-  (is (= (eval-defn '(defn foo222 [])) (symbol "Function foo created")) ))
+  (is (do
+        (ns-unmap-in-current-ns 'foo)
+        (= (eval-defn '(defn foo [])) (symbol "Function foo created"))) ))
 
 (defn eval-def [form]
   (deref (eval form)))
 
+(comment
+  (symbol (str *ns*)))
+
 (with-test
   (defn custom-eval [x]
-    (if (= 'defn (first x))
-      (if (seq? x)
-        (cond
-          (= 'defn (first x)) (eval-defn x)
-          (= 'def (first x)) (eval-def x)
-          :else (eval x))
-        (eval x))))
-  (is (= (custom-eval '(def foo 42)) 42)
-      (= (custom-eval '(defn foo [])) (symbol "Function foo created"))))
+    (if (seq? x)
+      (cond
+        (= 'defn (first x)) (eval-defn x)
+        (= 'def (first x)) (eval-def x)
+        :else (eval x))
+      (eval x)))
+  (is (= (custom-eval '(def foo 42)) 42))
+  (is (= (do
+        (ns-unmap-in-current-ns 'foo)
+        (custom-eval '(defn foo [])))
+      (symbol "Function foo created")))
+  (is (= (do
+           (ns-unmap-in-current-ns 'foo)
+           (custom-eval '(defn foo []))
+           (custom-eval '(defn foo [])))
+         (symbol (->> "Function foo updated")))))
