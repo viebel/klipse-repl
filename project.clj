e7a21155f8c1609d60a5430672e8e559d58827f9
Yehonathan Sharvit <viebel@gmail.com>
2018-11-02 10:25:45 +0200

    0.1.9 LICENSE

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
-(defproject viebel/klipse-repl "0.1.8"
+(defproject viebel/klipse-repl "0.1.9"
+  :url "https://github.com/viebel/klipse-repl"
+  :license {:name "Eclipse Public License"
+            :distribution :repo
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Beginners friendly Clojure REPL"
   :min-lein-version "2.8.1"
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
